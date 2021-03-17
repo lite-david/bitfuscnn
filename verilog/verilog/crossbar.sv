abb600f6cb61d8d645e6edd84c2014236d661521
lite-david <edwin.mascarenhas95@gmail.com>
2021-03-17 10:40:17 -0700

    crossbar 4-bit and 2-bit operation modes, best-case and worst-case tests

--- verilog/verilog/crossbar.sv
+++ verilog/verilog/crossbar.sv
@@ -71,6 +71,124 @@ always_comb begin
     next_buffer_write_enable[i] = 0;
   end // end for
   case(bitwidth)
+  2'b00:
+    begin
+      for(i = 0; i<256; i++) begin
+        // Compute bank that a row,column goes to 
+        bank = bank_from_rc(row_coordinate[i], column_coordinate[i]);
+        // If bank hasn't been sent to and, 
+        // the input hasn't already been sent in a previous cycle
+        if(!used_banks[bank] & !inputs_sent[i]) begin
+          next_buffer_row_write[bank] = row_coordinate[i];
+          next_buffer_column_write[bank] = column_coordinate[i];
+          // Truncate to 2 bits
+          case( i % 16 )
+            0:
+            begin
+              next_buffer_data_write[bank] = {6'b000000, products[i >> 4][3], products[i >> 4][0]}; 
+            end
+            1:
+            begin
+              next_buffer_data_write[bank] = {6'b000000, products[i >> 4][7], products[i >> 4][4]}; 
+            end
+            2:
+            begin
+              next_buffer_data_write[bank] = {6'b000000, products[i >> 4][11], products[i >> 4][8]}; 
+            end
+            3:
+            begin
+              next_buffer_data_write[bank] = {6'b000000, products[i >> 4][15], products[i >> 4][12]}; 
+            end
+            4:
+            begin
+              next_buffer_data_write[bank] = {6'b000000, products[i >> 4][19], products[i >> 4][16]}; 
+            end
+            5:
+            begin
+              next_buffer_data_write[bank] = {6'b000000, products[i >> 4][23], products[i >> 4][20]}; 
+            end
+            6:
+            begin
+              next_buffer_data_write[bank] = {6'b000000, products[i >> 4][27], products[i >> 4][24]}; 
+            end
+            7:
+            begin
+              next_buffer_data_write[bank] = {6'b000000, products[i >> 4][31], products[i >> 4][28]}; 
+            end
+            8:
+            begin
+              next_buffer_data_write[bank] = {6'b000000, products[i >> 4][35], products[i >> 4][32]}; 
+            end
+            9:
+            begin
+              next_buffer_data_write[bank] = {6'b000000, products[i >> 4][39], products[i >> 4][36]}; 
+            end
+            10:
+            begin
+              next_buffer_data_write[bank] = {6'b000000, products[i >> 4][43], products[i >> 4][40]}; 
+            end
+            11:
+            begin
+              next_buffer_data_write[bank] = {6'b000000, products[i >> 4][47], products[i >> 4][44]}; 
+            end
+            12:
+            begin
+              next_buffer_data_write[bank] = {6'b000000, products[i >> 4][51], products[i >> 4][48]}; 
+            end
+            13:
+            begin
+              next_buffer_data_write[bank] = {6'b000000, products[i >> 4][55], products[i >> 4][52]}; 
+            end
+            14:
+            begin
+              next_buffer_data_write[bank] = {6'b000000, products[i >> 4][59], products[i >> 4][56]}; 
+            end
+            15:
+            begin
+              next_buffer_data_write[bank] = {6'b000000, products[i >> 4][63], products[i >> 4][60]}; 
+            end
+          endcase
+          next_buffer_write_enable[bank] = 1;
+          used_banks[bank] = 1; // Mark bank as used 
+          next_inputs_sent[i] = 1; //Mark input as processed
+        end // end if
+      end // end for
+    end // end case for bitwidth = 0 ie 2 bit mode of operation
+  2'b01:
+    begin
+      for(i = 0; i<64; i++) begin
+        // Compute bank that a row,column goes to 
+        bank = bank_from_rc(row_coordinate[i], column_coordinate[i]);
+        // If bank hasn't been sent to and, 
+        // the input hasn't already been sent in a previous cycle
+        if(!used_banks[bank] & !inputs_sent[i]) begin
+          next_buffer_row_write[bank] = row_coordinate[i];
+          next_buffer_column_write[bank] = column_coordinate[i];
+          // Truncate to 4 bits
+          case( i % 4 )
+            0:
+            begin
+              next_buffer_data_write[bank] = {4'b0000, products[i >> 2][15], products[i >> 2][2:0]}; 
+            end
+            1:
+            begin
+              next_buffer_data_write[bank] = {4'b0000, products[i >> 2][31], products[i >> 2][18:16]}; 
+            end
+            2:
+            begin
+              next_buffer_data_write[bank] = {4'b0000, products[i >> 2][47], products[i >> 2][34:32]}; 
+            end
+            3:
+            begin
+              next_buffer_data_write[bank] = {4'b0000, products[i >> 2][63], products[i >> 2][50:48]}; 
+            end
+          endcase
+          next_buffer_write_enable[bank] = 1;
+          used_banks[bank] = 1; // Mark bank as used 
+          next_inputs_sent[i] = 1; //Mark input as processed
+        end // end if
+      end // end for
+    end // end case for bitwidth = 1 ie 4 bit mode of operation
   2'b10:
     begin
       for(i = 0; i<16; i++) begin
@@ -141,6 +259,34 @@ task route();
     inputs_sent <= next_inputs_sent | inputs_sent;
     //Handle stall signal and reset the inputs_sent 
     case(bitwidth)
+      2'b00:
+        begin
+          if (inputs_sent == 0) begin
+            crossbar_stall <= 0;
+          end
+          else begin
+            if (inputs_sent == {256{1'b1}}) begin
+              reset();
+            end
+            else begin
+              crossbar_stall <= 1;
+            end
+          end
+        end
+      2'b01:
+        begin
+          if (inputs_sent == 0) begin
+            crossbar_stall <= 0;
+          end
+          else begin
+            if (inputs_sent == {64{1'b1}}) begin
+              reset();
+            end
+            else begin
+              crossbar_stall <= 1;
+            end
+          end
+        end
       2'b10:
         begin
           if (inputs_sent == 0) begin
