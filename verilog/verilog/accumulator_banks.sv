69963c829563a2ad5c5e78ae1a133174246b3706
Max <maximilian.paulsen@gmail.com>
2021-03-16 19:41:09 -0700

    updated interface fo accumulator bank and uncommented tests

--- verilog/verilog/accumulator_banks.sv
+++ verilog/verilog/accumulator_banks.sv
@@ -20,9 +20,11 @@ module accumulator_banks #(
   input wire back_buffer_write_enable[BANK_COUNT],
 
   input wire [$clog2(BUFFER_WIDTH)-1:0] front_buffer_bank_entry,
+  input wire [$clog2(TILE_SIZE)-1:0] front_buffer_bank_read,
   output wire [SMALLEST_ELEMENT_WIDTH*4-1:0] front_buffer_data_read,
 
   input wire [$clog2(BUFFER_WIDTH)-1:0] back_buffer_bank_entry,
+  input wire [$clog2(TILE_SIZE)-1:0] back_bbuffer_bank_read,
   output wire [SMALLEST_ELEMENT_WIDTH*4-1:0] back_buffer_data_read
 );
   
